f3136517d8d08b6c8d2128a78312ff5f1a8ac690
Ali-Akber Saifee <ali@indydevs.org>
2014-02-11 08:24:14 +0800

    py2.6 compatible assert

--- tests/test_limit_granularities.py
+++ tests/test_limit_granularities.py
@@ -11,9 +11,9 @@ class GranularityTests(unittest.TestCase):
         self.assertEqual(limits.PER_YEAR(1).seconds, 60*60*24*30*12)
 
     def test_representation(self):
-        self.assertIn("1 per hour", str(limits.PER_HOUR(1)))
-        self.assertIn("1 per minute", str(limits.PER_MINUTE(1)))
-        self.assertIn("1 per second", str(limits.PER_SECOND(1)))
-        self.assertIn("1 per day", str(limits.PER_DAY(1)))
-        self.assertIn("1 per month", str(limits.PER_MONTH(1)))
-        self.assertIn("1 per year", str(limits.PER_YEAR(1)))
+        self.assertTrue("1 per hour" in str(limits.PER_HOUR(1)))
+        self.assertTrue("1 per minute" in str(limits.PER_MINUTE(1)))
+        self.assertTrue("1 per second" in str(limits.PER_SECOND(1)))
+        self.assertTrue("1 per day" in str(limits.PER_DAY(1)))
+        self.assertTrue("1 per month" in str(limits.PER_MONTH(1)))
+        self.assertTrue("1 per year" in str(limits.PER_YEAR(1)))
