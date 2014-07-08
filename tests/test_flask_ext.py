964923f5d1c860102650c9ae9ae90a1127711610
Ali-Akber Saifee <ali@indydevs.org>
2014-07-08 15:17:21 +0800

    test cases added to validate class based views (#11)
    * verifies that class based views using either flask.views.View,
    flask.views.MethodView or flask.ext.restful.Resource have rate limits
    respected when decorators are provided using the class 'decorators'
    variable.

--- tests/test_flask_ext.py
+++ tests/test_flask_ext.py
@@ -7,13 +7,15 @@ import time
 import logging
 import unittest
 from flask import Flask, Blueprint, request, current_app, make_response
+from flask.ext.restful import Resource
+from flask.views import View, MethodView
 import hiro
 import mock
 from flask.ext.limiter.errors import ConfigurationError
 from flask.ext.limiter.extension import Limiter, C
 from flask.ext.limiter.storage import MemcachedStorage
 from flask.ext.limiter.strategies import MovingWindowRateLimiter
-
+from flask.ext import restful
 
 class FlaskExtTests(unittest.TestCase):
 
@@ -514,3 +516,123 @@ class FlaskExtTests(unittest.TestCase):
                         cli.get("/", headers = {"internal": "true"}).status_code,
                         200
                     )
+
+
+    def test_pluggable_views(self):
+        app, limiter = self.build_app(
+            global_limits = ["1/hour"]
+        )
+        class Va(View):
+            methods = ['GET', 'POST']
+            decorators = [limiter.limit("2/second")]
+            def dispatch_request(self):
+                return request.method.lower()
+        class Vb(View):
+            methods = ['GET']
+            decorators = [limiter.limit("1/second, 3/minute")]
+            def dispatch_request(self):
+                return request.method.lower()
+        class Vc(View):
+            methods = ['GET']
+            def dispatch_request(self):
+                return request.method.lower()
+
+        app.add_url_rule("/a", view_func = Va.as_view("a"))
+        app.add_url_rule("/b", view_func = Vb.as_view("b"))
+        app.add_url_rule("/c", view_func = Vc.as_view("c"))
+        with hiro.Timeline().freeze() as timeline:
+            with app.test_client() as cli:
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(429, cli.post("/a").status_code)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(429, cli.get("/b").status_code)
+                self.assertEqual(200, cli.get("/c").status_code)
+                self.assertEqual(429, cli.get("/c").status_code)
+
+    def test_pluggable_method_views(self):
+        app, limiter = self.build_app(
+            global_limits = ["1/hour"]
+        )
+        class Va(MethodView):
+            decorators = [limiter.limit("2/second")]
+            def get(self):
+                return request.method.lower()
+            def post(self):
+                return request.method.lower()
+
+        class Vb(MethodView):
+            decorators = [limiter.limit("1/second, 3/minute")]
+            def get(self):
+                return request.method.lower()
+        class Vc(MethodView):
+            def get(self):
+                return request.method.lower()
+
+        app.add_url_rule("/a", view_func=Va.as_view("a"))
+        app.add_url_rule("/b", view_func=Vb.as_view("b"))
+        app.add_url_rule("/c", view_func=Vc.as_view("c"))
+
+        with hiro.Timeline().freeze() as timeline:
+            with app.test_client() as cli:
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(429, cli.get("/a").status_code)
+                self.assertEqual(429, cli.post("/a").status_code)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(429, cli.get("/b").status_code)
+                self.assertEqual(200, cli.get("/c").status_code)
+                self.assertEqual(429, cli.get("/c").status_code)
+
+    def test_flask_restful_resource(self):
+        app, limiter = self.build_app(
+            global_limits = ["1/hour"]
+        )
+        api = restful.Api(app)
+
+        class Va(Resource):
+            decorators = [limiter.limit("2/second")]
+            def get(self):
+                return request.method.lower()
+            def post(self):
+                return request.method.lower()
+
+        class Vb(Resource):
+            decorators = [limiter.limit("1/second, 3/minute")]
+            def get(self):
+                return request.method.lower()
+
+        class Vc(Resource):
+            def get(self):
+                return request.method.lower()
+
+        api.add_resource(Va, "/a")
+        api.add_resource(Vb, "/b")
+        api.add_resource(Vc, "/c")
+
+        with hiro.Timeline().freeze() as timeline:
+            with app.test_client() as cli:
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(200, cli.get("/a").status_code)
+                self.assertEqual(429, cli.get("/a").status_code)
+                self.assertEqual(429, cli.post("/a").status_code)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(200, cli.get("/b").status_code)
+                timeline.forward(1)
+                self.assertEqual(429, cli.get("/b").status_code)
+                self.assertEqual(200, cli.get("/c").status_code)
+                self.assertEqual(429, cli.get("/c").status_code)
+
