2be90927b8be9fde454f97e8e1f0fff1c080404a
Ali-Akber Saifee <ali@indydevs.org>
2017-08-18 09:48:06 +0800

    Make tag script py3 compatible

--- tag.sh
+++ tag.sh
@@ -5,10 +5,10 @@ read -p "new version:" new_version
 last_portion=$(grep -E "^Changelog$" HISTORY.rst -5 | grep -E "^\d+.\d+.\d+")
 changelog_file=/var/tmp/flask-ratelimit.newchangelog
 new_changelog_heading="${new_version} `date +"%Y-%m-%d"`"
-new_changelog_heading_sep=$(python -c "print '-'*len('$new_changelog_heading')")
+new_changelog_heading_sep=$(python -c "print('-'*len('$new_changelog_heading'))")
 echo $new_changelog_heading > $changelog_file
 echo $new_changelog_heading_sep >> $changelog_file
-python -c "print open('HISTORY.rst').read().replace('$last_portion', open('$changelog_file').read() +'\n' +  '$last_portion')" > HISTORY.rst.new 
+python -c "print(open('HISTORY.rst').read().replace('$last_portion', open('$changelog_file').read() +'\n' +  '$last_portion'))" > HISTORY.rst.new
 cp HISTORY.rst.new HISTORY.rst 
 vim HISTORY.rst
 if rst2html.py HISTORY.rst > /dev/null 
