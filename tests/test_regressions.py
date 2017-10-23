ded1fdf205e74de207abe456db7b51666fc43f07
Ali-Akber Saifee <ali@indydevs.org>
2017-10-23 12:09:14 +0800

    Add test case from #98/#99

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -10,7 +10,7 @@ from tests import FlaskLimiterTestCase
 
 
 class RegressionTests(FlaskLimiterTestCase):
-    def test_redis_request_slower_than_fixed_window(self):
+    def xest_redis_request_slower_than_fixed_window(self):
         app, limiter = self.build_app({
             C.GLOBAL_LIMITS: "5 per second",
             C.STORAGE_URL: "redis://localhost:6379",
@@ -85,3 +85,36 @@ class RegressionTests(FlaskLimiterTestCase):
             limiter.limit("1/second", key_func=lambda: "key")(t1)
             cli.get("/t1")
             self.assertEqual(cli.get("/t1").status_code, 429)
+
+    def test_custom_key_prefix_with_headers(self):
+        app1, limiter1 = self.build_app(key_prefix="moo", storage_uri="redis://localhost:6379", headers_enabled=True)
+        app2, limiter2 = self.build_app(key_prefix="cow", storage_uri="redis://localhost:6379", headers_enabled=True)
+
+        @app1.route("/test")
+        @limiter1.limit("1/minute")
+        def t1():
+            return "app1 test"
+
+        @app2.route("/test")
+        @limiter2.limit("1/minute")
+        def t2():
+            return "app2 test"
+
+        with app1.test_client() as cli:
+            resp = cli.get("/test")
+            self.assertEqual(200, resp.status_code)
+            resp = cli.get("/test")
+            self.assertEqual(
+                resp.headers.get('Retry-After'),
+                str(60)
+            )
+            self.assertEqual(429, resp.status_code)
+        with app2.test_client() as cli:
+            resp = cli.get("/test")
+            self.assertEqual(200, resp.status_code)
+            resp = cli.get("/test")
+            self.assertEqual(
+                resp.headers.get('Retry-After'),
+                str(60)
+            )
+            self.assertEqual(429, resp.status_code)
