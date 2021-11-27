5c11a65af1d51794133895ebe2de92861b0894cf
Ali-Akber Saifee <ali@indydevs.org>
2021-11-27 10:23:42 -0800

    Remove backward compatibility hack for exception subclass

--- flask_limiter/errors.py
+++ flask_limiter/errors.py
@@ -5,20 +5,8 @@ from pkg_resources import get_distribution
 from six import text_type
 from werkzeug import exceptions
 
-werkzeug_exception = None
-werkzeug_version = get_distribution("werkzeug").version
-if LooseVersion(werkzeug_version) < LooseVersion("0.9"):  # pragma: no cover
-    # sorry, for touching your internals :).
-    import werkzeug._internal
 
-    werkzeug._internal.HTTP_STATUS_CODES[429] = "Too Many Requests"
-    werkzeug_exception = exceptions.HTTPException
-else:
-    # Werkzeug 0.9 and up have an existing exception for 429
-    werkzeug_exception = exceptions.TooManyRequests
-
-
-class RateLimitExceeded(werkzeug_exception):
+class RateLimitExceeded(exceptions.TooManyRequests):
     """exception raised when a rate limit is hit.
 
     The exception results in ``abort(429)`` being called.
@@ -29,9 +17,11 @@ class RateLimitExceeded(werkzeug_exception):
 
     def __init__(self, limit):
         self.limit = limit
+
         if limit.error_message:
             description = (
                 limit.error_message
+
                 if not callable(limit.error_message)
                 else limit.error_message()
             )
