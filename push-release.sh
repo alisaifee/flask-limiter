c98cc9e8af9932640978b1281ab12495445f42f2
Ali-Akber Saifee <ali@indydevs.org>
2021-11-28 14:12:30 -0800

    Update push-stable script

--- push-release.sh
+++ push-release.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
+#!/bin/bash
 cur=$(git rev-parse --abbrev-ref HEAD)
-git checkout master 
-git push origin master --tags 
-git checkout stable
+git checkout master
+git push origin master --tags
+git checkout 2.x
 git merge master
-git push origin stable
+git push origin 2.x
 git checkout $cur
