08c11e38ef3003d6758d90c469d5725e374a7851
Ali-Akber Saifee <ali@indydevs.org>
2022-04-02 13:11:20 -0700

    Fix linting error

--- flask_limiter/__init__.py
+++ flask_limiter/__init__.py
@@ -1,13 +1,8 @@
 """Flask-Limiter extension for rate limiting."""
-from ._version import get_versions
+from . import _version
 from .errors import RateLimitExceeded
 from .extension import HEADERS, Limiter, RequestLimit
 
-__version__ = get_versions()["version"]
-del get_versions
-
 __all__ = ["RateLimitExceeded", "Limiter", "RequestLimit", "HEADERS"]
-
-from . import _version
 
 __version__ = _version.get_versions()["version"]
