91f8c283a6780c05a194d593907be97414488494
Ali-Akber Saifee <ali@indydevs.org>
2022-04-02 12:37:49 -0700

    Move publishing to CI

--- tag.sh
+++ tag.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-dryrun=0
-while [ "$1" != "" ]; do
-    case $1 in
-        -d | --dryrun )    dryrun=1
-                           ;;
-    esac
-    shift
-done
-
-rm -rf build dist
 last_tag=$(git tag | sort -nr | head -n 1)
 echo current version:$(python setup.py --version), current tag: $last_tag
 read -p "new version:" new_version
@@ -28,8 +18,6 @@ then
     git add HISTORY.rst
     git commit -m "updating changelog for  ${new_version}"
     git tag -s ${new_version} -m "tagging version ${new_version}"
-    python setup.py build sdist bdist_egg bdist_wheel
-    test $dryrun != 1 && twine upload dist/*
     rm HISTORY.rst.new
 else
     echo changelog has errors. skipping tag.
