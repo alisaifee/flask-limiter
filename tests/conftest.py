2aa111b7c2a7f6d8332cf47e43565c1157b3d2f9
Ali-Akber Saifee <ali@indydevs.org>
2022-04-06 05:42:21 -0700

    Cleanup test configs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import socket
+
 import pymemcache
 import pymongo
 import pytest
@@ -8,8 +10,20 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 
+def ping_socket(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((host, port))
+
+        return True
+    except Exception:
+        return False
+
+
 @pytest.fixture
 def redis_connection(docker_services):
+    docker_services.start("redis")
+    docker_services.wait_for_service("redis", 6379, ping_socket)
     r = redis.from_url("redis://localhost:46379")
     r.flushall()
 
@@ -18,11 +32,15 @@ def redis_connection(docker_services):
 
 @pytest.fixture
 def memcached_connection(docker_services):
+    docker_services.start("memcached")
+    docker_services.wait_for_service("memcached", 11211, ping_socket)
     return pymemcache.Client(("localhost", 31211))
 
 
 @pytest.fixture
 def mongo_connection(docker_services):
+    docker_services.start("mongodb")
+    docker_services.wait_for_service("mongodb", 27017, ping_socket)
     return pymongo.MongoClient("mongodb://localhost:47017")
 
 
@@ -42,5 +60,5 @@ def extension_factory():
 
 
 @pytest.fixture(scope="session")
-def docker_compose_file():
-    return "./docker-compose.yml"
+def docker_compose_files(pytestconfig):
+    return ["docker-compose.yml"]
