69eb322d67c3179381dc2f916fc368835792a0b2
Ali-Akber Saifee <ali@indydevs.org>
2022-08-11 16:02:56 -0700

    Stop maintaining 2.x branch

--- push-release.sh
+++ push-release.sh
@@ -2,9 +2,6 @@
 cur=$(git rev-parse --abbrev-ref HEAD)
 git checkout master
 git push origin master --tags
-git checkout 2.x
-git merge master
-git push origin 2.x
 git checkout stable
 git merge master
 git push origin stable
