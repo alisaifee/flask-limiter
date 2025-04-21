2fbfd85cc3ccf2ce949b95a99bc79e009ce7dd45
Ali-Akber Saifee <ali@indydevs.org>
2025-04-20 18:28:16 -0700

    Fix incorrect docstring for decorator key_func param

--- flask_limiter/extension.py
+++ flask_limiter/extension.py
@@ -512,8 +512,7 @@ class Limiter:
         :param limit_value: rate limit string or a callable that returns a
          string. :ref:`ratelimit-string` for more details.
         :param key_func: function/lambda to extract the unique
-         identifier for the rate limit. defaults to remote address of the
-         request.
+         identifier for the rate limit.
         :param per_method: whether the limit is sub categorized into the
          http method of the request.
         :param methods: if specified, only the methods in this list will
@@ -598,8 +597,7 @@ class Limiter:
         :param scope: a string or callable that returns a string
          for defining the rate limiting scope.
         :param key_func: function/lambda to extract the unique
-         identifier for the rate limit. defaults to remote address of the
-         request.
+         identifier for the rate limit.
         :param per_method: whether the limit is sub categorized into the
          http method of the request.
         :param methods: if specified, only the methods in this list will
